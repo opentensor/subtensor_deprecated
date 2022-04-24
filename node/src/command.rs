e9702fe55d9201e3a2b61eff4fc98b3925e1f0c4
Ala Shaabana <alashaabana@Alas-MacBook-Pro-2.local>
2022-04-24 17:50:29 -0400

    Added spec

--- node/src/command.rs
+++ node/src/command.rs
@@ -35,7 +35,7 @@ impl SubstrateCli for Cli {
 	fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
 		Ok(match id {
 			"dev" => Box::new(chain_spec::development_config()?),
-			"" | "local" => Box::new(chain_spec::local_testnet_config()?),
+			"" | "nakamoto" => Box::new(chain_spec::nakamoto_mainnet_config()?),
 			path =>
 				Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(path))?),
 		})
