f8dd20f2c1bd0fcbb80454d5ff6a3d146f93b141
Ala Shaabana <alashaabana@Alas-MacBook-Pro-2.local>
2022-05-25 15:52:17 -0400

    Fixed milliseconds per block to 12 instead of 6

--- pallets/subtensor/src/registration.rs
+++ pallets/subtensor/src/registration.rs
@@ -19,6 +19,9 @@ impl<T: Config> Pallet<T> {
         hotkey: T::AccountId, 
         coldkey: T::AccountId 
     ) -> dispatch::DispatchResult {
+
+        log::info!("Registration here!");
+
         // --- Check the callers hotkey signature.
         ensure_signed(origin)?;
 
